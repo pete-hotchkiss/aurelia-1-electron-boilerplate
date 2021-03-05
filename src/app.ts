f60a4cf59d1a50876aa08227cd21f821a1c1c69a
pete hotchkiss <pete.hotchkiss@gmail.com>
2021-03-05 13:34:16 +0000

    basic routing working in localdevb

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,54 @@
-// import "home.scss";
+import { autoinject } from "aurelia-framework";
+import { Router, RouterConfiguration, NavModel } from "aurelia-router";
+import { PLATFORM } from "aurelia-pal";
+
+@autoinject
 export class App {
-  public message = 'Hello World!';
+
+  routes: any[];
+  router: Router;
+  message: string;
+
+  constructor() {
+    this.message = 'how dooze!';
+    this.routes = [{
+      route: [ "", "home" ],
+      name: "home-panel",
+      moduleId: PLATFORM.moduleName("views/home-panel"),
+      nav: true,
+      title: "Home",
+      settings: {
+        requireAdmin: false,
+        moduleAuthRequired: false,
+        sortOrder: 1,
+      }
+    },
+    {
+      route: "a",
+      name: "panel-a",
+      moduleId: PLATFORM.moduleName("views/panel-a"),
+      nav: true,
+      title: "Panel A",
+      settings: {
+        requireAdmin: false,
+        moduleAuthRequired: false,
+        sortOrder: 1,
+      }
+    }];
+  }
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    console.log("configure router called...");
+    this.router = router;
+    config.options.root = "/";
+    config.options.pushState = false;
+    // this.router.baseUrl("/");
+    const handleUnknownRoutes = () => {
+      console.log("route not found...")
+      return { route: "not-found", moduleId: "not-found" };
+    };
+
+    config.mapUnknownRoutes(handleUnknownRoutes);
+    config.map(this.routes);
+  }
 }
