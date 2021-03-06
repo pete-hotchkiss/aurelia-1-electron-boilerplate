778c1c8647154fa64df1e40117533c79643a8417
pete hotchkiss <pete.hotchkiss@gmail.com>
2021-03-06 00:10:06 +0000

    👷 conditional app roots for local server and electron enviornments

--- src/app.ts
+++ src/app.ts
@@ -1,68 +1,26 @@
 import { ipcRenderer } from "electron";
-import { autoinject } from "aurelia-framework";
+import { autoinject, useView } from "aurelia-framework";
 import { Router, RouterConfiguration, NavModel } from "aurelia-router";
 import { PLATFORM } from "aurelia-pal";
+import { BaseApp } from "./base-app";
 
-@autoinject
-export class App {
-
-  routes: any[];
-  router: Router;
-  message: string;
+@autoinject()
+@useView("./base-app.html")
+export class App extends BaseApp {
 
   constructor() {
-    this.message = 'how dooze!';
-    this.routes = [
-    {
-      route: ["", "app-home"],
-      name: "home-panel",
-      moduleId: PLATFORM.moduleName("views/home-panel"),
-      nav: true,
-      title: "Home",
-      settings: {
-        requireAdmin: false,
-        moduleAuthRequired: false,
-        sortOrder: 1,
-      }
-    },
-    {
-      route: "app-loading",
-      name: "panel-a",
-      moduleId: PLATFORM.moduleName("views/panel-a"),
-      nav: true,
-      title: "Panel A",
-      settings: {
-        requireAdmin: false,
-        moduleAuthRequired: false,
-        sortOrder: 1,
-      }
-    }];
-
-    console.log("where..", globalThis.electron);
-
+    super()
+    //
     ipcRenderer.on(
       "onLoadingWindowLoaded",
-      (event: any, ...args) => this.onLoadingWindowLoaded(args));
+      (event: any, ...args) => this.onLoadingWindowLoaded(args)
+    );
   }
 
   onLoadingWindowLoaded(args: {route: string}[]) {
+    console.log("this", this, this.router);
     /** event gets an array of arguments which detail the */
     /** route the app should navigate to */
     this.router.navigate(args[0].route);
-  }
-
-  configureRouter(config: RouterConfiguration, router: Router) {
-    console.log("configure router called...");
-    this.router = router;
-    config.options.root = "/";
-    config.options.pushState = false;
-    // this.router.baseUrl("/");
-    const handleUnknownRoutes = () => {
-      console.log("route not found...")
-      return { route: "not-found", moduleId: "not-found" };
-    };
-
-    config.mapUnknownRoutes(handleUnknownRoutes);
-    config.map(this.routes);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import {Aurelia} from 'aurelia-framework';
 import * as environment from '../config/environment.json';
-import {PLATFORM} from 'aurelia-pal';
+import { PLATFORM } from 'aurelia-pal';
+import { isElectron } from "utils/is-electron";
 
 export function configure(aurelia: Aurelia): void {
+
+  console.log("...", process.env.BUILD_ENVIRONMENT);
+
+  if (isElectron()) {
+    console.log("we're in eleectron");
+  } else {
+    console.log("...", process.env.BUILD_ENVIRONMENT);
+  }
+
   aurelia.use
     .standardConfiguration()
     .feature(PLATFORM.moduleName('resources/index'));
@@ -13,5 +23,10 @@ export function configure(aurelia: Aurelia): void {
     aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
   }
 
-  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+  aurelia.start()
+  .then(
+    () => aurelia.setRoot(
+      PLATFORM.moduleName(process.env.BUILD_ENVIRONMENT))
+  );
+  // process.env.BUILD_ENVIRONMENT
 }
